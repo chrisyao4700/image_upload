735b3411e7eb5dcecfd044ac9f225d4ba0ea7224
Xian <josiexianli@gmail.com>
2018-01-12 11:20:10 -0800

    INIT

---
+++ public/javascripts/index_vc.js
@@ -0,0 +1,30 @@
+//here is something for view controller
+var $ = require('jQuery');
+
+function uploadByForm() {
+    //用form 表单直接 构造formData 对象; 就不需要下面的append 方法来为表单进行赋值了。
+    var formData = new FormData($("#myForm")[0]);
+    var url = "http://localhost:3000/photos/myPhotos";
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+
+        /**
+         * 必须false才会避开jQuery对 formdata 的默认处理
+         * XMLHttpRequest会对 formdata 进行正确的处理
+         */
+        processData: false,
+        /**
+         *必须false才会自动加上正确的Content-Type
+         */
+        contentType: false,
+        success: function (responseStr) {
+            alert(responseStr.newPath);
+            $("img").attr({"src": responseStr.newPath}).prependTo($("body"));
+        },
+        error: function (responseStr) {
+            alert(responseStr.newPath);
+        }
+    });
+}
